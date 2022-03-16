fa7d5ee1c595650c221a092330ffd111534c9c46
Raman Shakya <50479105+Ramanshk@users.noreply.github.com>
2022-03-16 21:07:55 +0545

    Homework task completed for ScrollingText app

---
+++ ScrollingText2/app/src/main/java/com/example/scrollingtext2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.scrollingtext2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import scrollingtext2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
